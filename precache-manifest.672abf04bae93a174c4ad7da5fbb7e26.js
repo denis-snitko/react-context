d32a84686428318bd035679913b91c104fc935c2
Denis Snitko <denissnitko@gmail.com>
2021-10-09 10:22:11 +0300

    Updates

---
+++ precache-manifest.672abf04bae93a174c4ad7da5fbb7e26.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c80c4fa7bd5d2e1aeea3a16d56d21ac",
+    "url": "/react-context/index.html"
+  },
+  {
+    "revision": "b9b606e6dc8460b1c754",
+    "url": "/react-context/static/css/2.5544c887.chunk.css"
+  },
+  {
+    "revision": "967dcdc31fe4777341cb",
+    "url": "/react-context/static/css/main.14f1c0e1.chunk.css"
+  },
+  {
+    "revision": "b9b606e6dc8460b1c754",
+    "url": "/react-context/static/js/2.3dfb71c5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-context/static/js/2.3dfb71c5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "967dcdc31fe4777341cb",
+    "url": "/react-context/static/js/main.c279a65f.chunk.js"
+  },
+  {
+    "revision": "d3035a48220f89398012",
+    "url": "/react-context/static/js/runtime-main.8bf01b1a.js"
+  }
+]);
